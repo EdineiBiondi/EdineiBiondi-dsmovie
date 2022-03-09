577ae9b6d377bc5b2efba84955e1b6be93b71d2c
EdineiBiondi <edinei.biondi@gmail.com>
2022-03-09 11:42:19 -0300

    MovieCard

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -3,17 +3,18 @@ import './styles.css';
 
 function Pagination() {
 
-    return (<div className="dsmovie-pagination-container">
-        <div className="dsmovie-pagination-box">
-            <button className="dsmovie-pagination-button" disabled={true} >
-                <Arrow />
-            </button>
-            <p>{`${1} de ${3}`}</p>
-            <button className="dsmovie-pagination-button" disabled={false} >
-                <Arrow className="dsmovie-flip-horizontal" />
-            </button>
+    return (
+        <div className="dsmovie-pagination-container">
+            <div className="dsmovie-pagination-box">
+                <button className="dsmovie-pagination-button" disabled={true} >
+                    <Arrow />
+                </button>
+                <p>{`${1} de ${3}`}</p>
+                <button className="dsmovie-pagination-button" disabled={false} >
+                    <Arrow className="dsmovie-flip-horizontal" />
+                </button>
+            </div>
         </div>
-    </div>
 
     );
 }
