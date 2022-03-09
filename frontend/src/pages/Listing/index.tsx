0eeac5d9481f8083cb7b7b392ffdc77c27417d56
EdineiBiondi <edinei.biondi@gmail.com>
2022-03-09 13:32:41 -0300

    Navigation buttons

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -10,7 +10,7 @@ function Listing() {
             <div className='container'>
                 <div className='row'>
                     <div className='col-sm-6 col-lg-4 col-xl-3 mb-3'>
-                        <MovieCard />
+                        <MovieCard />                       
                     </div>
                     <div className='col-sm-6 col-lg-4 col-xl-3 mb-3'>
                         <MovieCard />
